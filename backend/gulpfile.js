1a4f41ced23bdb39a2a2fe5f23c4dc961f53c0f1
Lt <trepafi@gmail.com>
2014-08-21 19:11:07 +0200

    Updating gulpile

--- backend/gulpfile.js
+++ backend/gulpfile.js
@@ -5,12 +5,12 @@ var rename = require('gulp-rename');
 var browserify = require('gulp-browserify');
 
 gulp.task('bundle', function() {
-	return gulp.src('app/**/*.js')
+	return gulp.src('api/*.js')
 			.pipe(browserify({
 				debug: (process.env.NODE_ENV !== 'production')
 			}))
-			.pipe(rename('admin.js'))
-			.pipe(gulp.dest('admin/public/js'))
+			.pipe(rename('api.js'))
+			.pipe(gulp.dest('dist/'))
 });
 
 gulp.task('nodemon', function() {
@@ -19,21 +19,11 @@ gulp.task('nodemon', function() {
 	})
 });
 
-// gulp.task('celery', function() {
-// 	spawn('npm', ['run', 'celery'], {
-// 		stdio: 'inherit'
-// 	})
-// });
-
 gulp.task('watch', function() {
-	gulp.watch('admin/app/**/*.js', ['bundle']);
-	gulp.watch('node_modules/ramson-client/src/**/*.js', ['bundle']);
+	gulp.watch('api/*.js', ['bundle']);
 });
 
 gulp.task('default', ['bundle']);
 gulp.task('run', ['bundle'], function() {
 	gulp.start('nodemon', 'watch');
-});
-gulp.task('dev', ['bundle'], function() {
-	gulp.start('celery', 'nodemon', 'watch');
 });
