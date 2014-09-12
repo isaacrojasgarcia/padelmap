575eeeefc28ba5e5680173d0a035945ec770fcbf
Lt <trepafi@gmail.com>
2014-09-13 00:28:51 +0200

    Updating gulp config file

--- backend/gulpfile.js
+++ backend/gulpfile.js
@@ -4,14 +4,35 @@ var spawn = require('child_process').spawn;
 var rename = require('gulp-rename');
 var browserify = require('gulp-browserify');
 
-gulp.task('bundle', function() {
-	return gulp.src('api/*.js')
-			.pipe(browserify({
-				debug: (process.env.NODE_ENV !== 'production')
-			}))
-			.pipe(rename('api.js'))
-			.pipe(gulp.dest('dist/'))
+var cssmin = require('gulp-cssmin');
+var concat = require('gulp-concat');
+var clean = require('gulp-clean');
+
+// gulp.task('bundle', function() {
+// 	return gulp.src('app/*.js')
+// 			.pipe(browserify({
+// 				debug: (process.env.NODE_ENV !== 'production')
+// 			}))
+// 			.pipe(rename('app.js'))
+// 			.pipe(gulp.dest('dist/'))
+// });
+
+// Styles stuff
+gulp.task('styles', function () {
+    gulp.src('app/**/*.css')
+    	.pipe(concat('app.min.css'))
+        .pipe(cssmin())
+        // .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/css'));
 });
+
+// Clean all builds
+gulp.task('clean', function() {
+  return gulp.src(['dist'], {read: false})
+    .pipe(clean());
+});
+
+
 
 gulp.task('nodemon', function() {
 	spawn('nodemon', [], {
@@ -20,11 +41,14 @@ gulp.task('nodemon', function() {
 });
 
 gulp.task('watch', function() {
-	gulp.watch('api/*.js', ['bundle']);
+	gulp.watch('app/*.js', ['bundle']);
 });
 
-gulp.task('default', ['bundle']);
-
-gulp.task('run', ['bundle'], function() {
+gulp.task('run', ['clean'], function() {
+	gulp.start('styles');
 	gulp.start('nodemon', 'watch');
 });
+
+
+
+gulp.task('default', ['styles']);
